133d50c7d152df9ff8277b5498f888b1a0a2fd4d
philippe28 <phadc@hotmail.com>
2016-09-30 22:22:14 -0300

    adicionando whitenoise no projeto

--- core/views.py
+++ core/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Category
+
 # Create your views here.
 
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def contact(request):
-    return render(request,'contact.html')
-
-def product_list(request):
-    return render(request,'product_list.html')
-
-
-def product(request):
-    return render(request,'product.html')
+    return render(request, 'contact.html')
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'catalog',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #apps
+                'catalog.context_processors.categories',
             ],
         },
     },
@@ -120,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
