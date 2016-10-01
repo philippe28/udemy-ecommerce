c1fc49af67a9bc0f99424d07ce762a1eb1f2a2b3
philippe28 <phadc@hotmail.com>
2016-09-30 22:49:17 -0300

    bug settings database 6

---
+++ catalog/context_processors.py
@@ -0,0 +1,8 @@
+
+from .models import Category
+
+
+def categories(request):
+    return{
+        'categories': Category.objects.all()
+    }
---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from catalog import views
+
+
+urlpatterns = [
+    url(r'^$', views.product_list, name='product_list'),
+    url(r'^(?P<slug>[\w_-]+)/$', views.category, name='category'),
+    url(r'^produtos/(?P<slug>[\w_-]+)/$', views.product, name='product'),
+]
