c8d7ad6d1eaa7dba91083c6ed91e99b544e185b7
philippe28 <phadc@hotmail.com>
2016-09-30 23:05:25 -0300

    bug sem estilo4

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve as serve_static
 
 from core import views
 from catalog import views as views_catalog
