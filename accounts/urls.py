be7b54ecc7517f689bc78ae275e7db62004d508a
philippe28 <phadc@hotmail.com>
2016-11-05 16:21:47 -0200

    sessao 4 aula 45

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^registro/$', views.register, name='register'),
+
+]
--- core/views.py
+++ core/views.py
@@ -33,12 +33,3 @@ def contact(request):
         'success': success
     }
     return render(request, 'contact.html', context)
-
-
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'register.html'
-    model = User
-    success_url = reverse_lazy('index')
-
-register = RegisterView.as_view()
