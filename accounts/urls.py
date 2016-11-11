60bc8db4da54b7164fbb35a226dd5329d38d363f
philippe28 <phadc@hotmail.com>
2016-11-10 22:10:32 -0200

    sessao 4 aula 48

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^registro/$', views.register, name='register'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
+
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
 from .forms import UserAdminCreationForm
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'accounts/index.html'
 
 
 class RegisterView(CreateView):
@@ -14,4 +21,5 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 
+index = IndexView.as_view()
 register = RegisterView.as_view()
