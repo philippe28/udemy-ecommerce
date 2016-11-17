b02de0fae2d9b3cbe4355e6b97766fb0239b6b80
philippe28 <phadc@hotmail.com>
2016-11-16 22:10:45 -0200

    sessao 5 aula 55

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import View, TemplateView, CreateView
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -28,6 +29,8 @@ def contact(request):
     if form.is_valid():
         form.send_mail()
         success = True
+    elif request.method == 'POST':
+        messages.error(request, 'Formulário inválido')
     context = {
         'form': form,
         'success': success
